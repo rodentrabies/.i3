207c97b47a716f0872bd109be4f15a70bba69b18
whythat <whythat@protonmail.com>
2017-05-28 02:43:03 +0300

    Use coinmarketcap API instead of cryptonator

--- scripts/cryptoprice.sh
+++ scripts/cryptoprice.sh
@@ -5,10 +5,10 @@
 # must be a official cryptocurrency symbol (BTC, XMR, ETC, etc)
 #------------------------------------------------------------------------------
 
-data=$(curl -sL https://api.cryptonator.com/api/ticker/$1-usd)
+data=$(curl -sL https://api.coinmarketcap.com/v1/ticker/$1)
 
 if [[ ! -z $data ]]; then
-    echo '$'$(echo $data | jq .ticker.price | tr -d '"' | xargs printf "%.*f" 2) 
+    echo '$'$(echo $data | jq .[0].price_usd | tr -d '"' | xargs printf "%.*f" 2) 
 else
     echo "N/A"
 fi
