da2fdf2fbb8a1e5def99fd0836216d6aa329a464
whythat <whythat@protonmail.com>
2017-10-15 12:06:14 +0300

    Exit launcher mode on launching an app

--- scripts/dwarfpool.sh
+++ scripts/dwarfpool.sh
@@ -6,6 +6,8 @@
 DETP='[0-9]+.[0-9]+ XMR</span>\nCurrent balance<br>'
 CUTP='[0-9]\+.[0-9]\+'
 
+DP_XMR_ADDRESS=$(cat $HOME/.crypto/wallets/monero/mining/mining.address.txt)
+
 if [ ! -z ${DP_XMR_ADDRESS+x} ]; then
 	MINING_ADDR=https://dwarfpool.com/xmr/address?wallet=$DP_XMR_ADDRESS
 	curl -s -X GET $MINING_ADDR | pcregrep -M "$DETP" | grep -o "$CUTP"
