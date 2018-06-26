8cab05a23beac30d816a31061d77d463b09eb39b
whythat <whythat@protonmail.com>
2018-06-26 16:00:52 +0300

    Use directory-per-component structure
    This simplifies the setup process for components that look for
    configuration in hardcoded locations (see setup.sh for examples).

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Setup i3 environment by installing required packages and setting
+# configuring components/tools to work properly.
+
+# set config directory for twmn
+ln -s ~/.i3/components/twmn ~/.config/twmn
